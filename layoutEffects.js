6be6058f1e72409d3bf55083f3d14d1fdf2bbc4c
JolitaArnau <jolita.arnaudova@gmail.com>
2017-06-05 15:13:27 +0200

    Implemented Points Counter and Skills

---
+++ layoutEffects.js
@@ -0,0 +1,32 @@
+function showFullStar() {
+    var fullStar = document.getElementById('hidden-full-star');
+    fullStar.style.visibility = 'visible';
+}
+
+function showHalfStar() {
+    var halfStar = document.getElementById('hidden-half-star');
+    halfStar.style.visibility = 'visible';
+}
+
+function showFullStarIntern() {
+    var fullStarIntern = document.getElementById('hidden-fullStar');
+    fullStarIntern.style.visibility = 'visible';
+    debugger;
+}
+
+
+
+$(document).ready(function () {
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            },
+            {
+                duration: 4000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+    });
+});
